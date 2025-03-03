e79c5723621245bc680a0bd98336576a2dfab9fd
italolimma <italoaslima@live.com>
2025-03-03 15:46:11 -0300

    criação de uma nova branch para testes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args){
         System.out.println("Avanade Decola Tech 2025");
+        System.out.println("Nova Branch e teste git");
     }
 }
